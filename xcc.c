cd6970bf1cb08f3aa694dd9af031ee44868dddf2
KentaKudo <yd37574@gmail.com>
2019-05-19 09:25:36 +0100

    Impl: +/- unary operators

--- xcc.c
+++ xcc.c
@@ -94,6 +94,7 @@ int consume(int ty) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *term();
 
 Node *expr() {
@@ -110,16 +111,24 @@ Node *expr() {
 }
 
 Node *mul() {
-  Node *node = term();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node('*', node, term());
+      node = new_node('*', node, unary());
     else if (consume('/'))
-      node = new_node('/', node, term());
+      node = new_node('/', node, unary());
     else
       return node;
   }
+}
+
+Node *unary() {
+  if (consume('+'))
+    return term();
+  if (consume('-'))
+    return new_node('-', new_node_num(0), term());
+  return term();
 }
 
 Node *term() {
